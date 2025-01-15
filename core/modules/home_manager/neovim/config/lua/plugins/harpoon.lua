b1c0d2e7f81f6f228c11693d9b2c40ec99c6dbaf
Alexandru Șeremet <seremet.alexandru.work@gmail.com>
2025-01-15 13:24:02 +0100

    Adjusted config for linux

--- core/modules/home_manager/neovim/config/lua/plugins/harpoon.lua
+++ core/modules/home_manager/neovim/config/lua/plugins/harpoon.lua
@@ -17,28 +17,28 @@ local keys = function()
       mode = "n",
     },
     {
-      "<C-A-h>",
+      "<S-A-h>",
       function()
         harpoon:list():select(1)
       end,
       mode = "n",
     },
     {
-      "<C-A-j>",
+      "<S-A-j>",
       function()
         harpoon:list():select(2)
       end,
       mode = "n",
     },
     {
-      "<C-A-k>",
+      "<S-A-k>",
       function()
         harpoon:list():select(3)
       end,
       mode = "n",
     },
     {
-      "<C-A-l>",
+      "<S-A-l>",
       function()
         harpoon:list():select(4)
       end,
